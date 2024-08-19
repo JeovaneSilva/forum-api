63ca347a393a6cef1dee09055000cc0274e812a3
jeovane lima <145929373+JeovaneSilva@users.noreply.github.com>
2024-08-18 21:01:19 -0300

    first commit

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { Prisma, User as UserModel } from '@prisma/client';
+import { UserService } from './user.service';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService){
+
+    }
+
+    @Post()
+  async signupUser(
+    @Body() userData: Prisma.UserCreateInput,
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+
+  @Get()
+    async findAll() {
+        return this.userService.findAll();
+    }
+
+  @UseGuards(AuthGuard)
+  @Get(':id')
+  async getUser(@Param('id') id: string): Promise<UserModel>{
+    return this.userService.user({ id: Number(id) });
+  }
+
+  @UseGuards(AuthGuard)
+  @Patch(':id')
+  async updateUser(
+    @Body() userData: Prisma.UserUpdateInput,
+    @Param('id') id: string): Promise<UserModel> {
+    return this.userService.updateUser({
+      where: {id:Number(id)}, 
+      data: userData
+    });
+  }
+
+  @UseGuards(AuthGuard) 
+  
+  @Delete(':id')
+  async deleteuser(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.deleteUser({ id: Number(id) });
+  }
+}
